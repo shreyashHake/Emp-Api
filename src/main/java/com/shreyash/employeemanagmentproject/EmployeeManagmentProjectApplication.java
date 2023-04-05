6dc3e1e5068a2e3560ae762443f711d35635fe7e
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-04-05 10:43:57 +0530

    Initial Commit

---
+++ src/main/java/com/shreyash/employeemanagmentproject/EmployeeManagmentProjectApplication.java
@@ -0,0 +1,13 @@
+package com.shreyash.employeemanagmentproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagmentProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagmentProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyash/employeemanagmentproject/EmployeeManagmentProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyash.employeemanagmentproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagmentProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
